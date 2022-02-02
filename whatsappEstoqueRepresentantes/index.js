dfccfd5fe78f053d77649d2b40bf70eaa89069be
Rodrigo <devstackpro@gmail.com>
2022-02-02 09:44:52 -0300

    Alteração da variavel nome

--- whatsappEstoqueRepresentantes/index.js
+++ whatsappEstoqueRepresentantes/index.js
@@ -7,13 +7,13 @@ const month = ["January","February","March","April","May","June","July","August"
 let name = month[now.getMonth()];
 
 
-nome = 'GOLD' + now.getDate()  + name + now.getFullYear() + now.getHours() + now.getMinutes() +'.png';
+gold = 'GOLD' + now.getDate()  + name + now.getFullYear() + now.getHours() + now.getMinutes() +'.png';
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://94550ac37bb5.sn.mynetname.net:15001/dev/dgb_/prodash/estoque_pronta_entrega_gold.php');
-  await page.screenshot({ path: nome, fullPage:true });
+  await page.screenshot({ path: gold, fullPage:true });
   var dataAtual = new Date();
   //console.log(dataAtual);
   //console.log(name);
